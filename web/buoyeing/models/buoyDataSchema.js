48e533c4cdfad464812429072d77d4708749ebd9
filio <ftitopoulos@gmail.com>
2024-03-14 01:54:50 +0200

    build:model for buoy data

---
+++ web/buoyeing/models/buoyDataSchema.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const buoyDataSchema = new Schema({
+  timestamp: { type: Date, index: true },
+  location: {
+    type: { type: String, default: 'Point' },
+    coordinates: [Number] // [longitude, latitude]
+  },
+  movement: {
+    horizontal: Number,
+    vertical: Number,
+  },
+  wind: {
+    direction: Number, // ? Wind direction in degrees from true north ?
+    strength: Number, // Wind speed (m/s)
+  },
+  air: {
+    temp: Number, // Air temperature (v celzii)
+    humidity: Number, // Humidity %
+  },
+  water: {
+    temp: Number, // Water temperature (v celzii)
+    salinity: Number, // Salinity ?(PSU ili PPT)?
+  },
+  light: Number, // Light intensity (Lux)
+  atmosphericPressure: Number, // Atmospheric pressure ? (hPa ili mBar) ?
+}, { timestamps: true });
+
+// Ensure the index for geo-location queries
+buoyDataSchema.index({ location: '2dsphere' });
+
+// Model creation
+const BuoyData = model('BuoyData', buoyDataSchema);
+
+export default BuoyData;

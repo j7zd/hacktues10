388db9f50ec27ff344594f6950a389f5d523c3d7
filio <ftitopoulos@gmail.com>
2024-03-14 02:08:37 +0200

    add:database connection

---
+++ web/buoyeing/utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // track connection status
+
+export const connectToDatabase = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('=> using existing database connection');
+        return;
+    }
+
+    try{
+        await mongoose.connect(process.env.DB_URI, {
+            dbName: process.env.DB_DATABASE_NAME,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        isConnected = true;
+
+        console.log('=> using new database connection');
+    } catch (error){
+        console.log('=> error connecting to database: ', error);
+    }
+};

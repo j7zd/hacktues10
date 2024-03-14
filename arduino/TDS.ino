16d1acd99816d3792e1f4dbae2dbe55de161252c
j7zd <j7zd@github.com>
2024-03-15 00:55:15 +0200

    refactoring

---
+++ arduino/TDS.ino
@@ -0,0 +1,20 @@
+#include <EEPROM.h>
+#include "GravityTDS.h"
+
+// This is a class for mesuring water cuality. PPM is the unit of mesurement
+class TDS {
+    private:
+        GravityTDS gravityTds;
+    public:
+        TDS(int pin) {
+            gravityTds.setPin(pin);
+            gravityTds.setAref(5.0); 
+            gravityTds.setAdcRange(1024);  
+            gravityTds.begin(); 
+        }
+
+        float getTDS() {
+            gravityTds.update();  
+            return gravityTds.getTdsValue();  
+        }
+}

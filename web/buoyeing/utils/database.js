de6b80114c14dbb23c10fa3f68d674c18c8ea20a
filio <ftitopoulos@gmail.com>
2024-03-14 03:03:53 +0200

    fix:db name

--- web/buoyeing/utils/database.js
+++ web/buoyeing/utils/database.js
@@ -12,7 +12,7 @@ export const connectToDatabase = async () => {
 
     try{
         await mongoose.connect(process.env.DB_URI, {
-            dbName: process.env.DB_DATABASE_NAME,
+            dbName: process.env.DB_NAME,
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });

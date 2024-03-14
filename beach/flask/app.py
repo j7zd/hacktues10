780f0667383ea922aa850beab74dc27f934d7359
j7zd <j7zd@github.com>
2024-03-14 23:20:26 +0200

    add: code for the raspberry pi

---
+++ beach/flask/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from os import getenv
+
+API_KEY = getenv("API_KEY")
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def receive_data():
+    data = request.get_data()
+    # if request.headers.get("api_key") != API_KEY:
+    #     return "Unauthorized", 401
+    print(data)

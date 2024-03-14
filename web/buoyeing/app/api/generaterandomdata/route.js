6d0625a41cbb22ff82991bec13a085d648e0925b
filio <ftitopoulos@gmail.com>
2024-03-14 03:41:04 +0200

    fix:random data

--- web/buoyeing/app/api/generaterandomdata/route.js
+++ web/buoyeing/app/api/generaterandomdata/route.js
@@ -1,10 +1,12 @@
 import { connectToDatabase } from '@/utils/database';
+import Buoy from '@/models/buoySchema';
 import BuoyData from '@/models/buoyDataSchema';
 import { NextResponse } from "next/server";
 
 // A helper function to generate random data
-const generateRandomData = () => {
+const generateRandomData = (buoyUid) => {
     return {
+        buoyUID: buoyUid,
         timestamp: new Date(),
         location: {
             type: 'Point',
@@ -33,13 +35,33 @@ const generateRandomData = () => {
         atmosphericPressure: Math.random() * 20 + 980, // Assuming hPa, 980 to 1000
     };
 };
+// ! DA SE PROMENI AKO SHTE SHTE SE SLAGA RANDOM DATA !
+const BUOY_UID = 'cfb4b6d0-8cd0-4c3f-8c92-c555c6950049'; // DA SE PROMENI AKO SHTE SHTE SE SLAGA RANDOM DATA
+const NAME = 'Treta shamandura buoy'; // DA SE PROMENI AKO SHTE SHTE SE SLAGA RANDOM DATA
+const COORDS = [-70.0, 40.0]; // DA SE PROMENI AKO SHTE SHTE SE SLAGA RANDOM DATA
+// ! DA SE PROMENI AKO SHTE SHTE SE SLAGA RANDOM DATA !
 
 export async function POST(req, res) {
     if (process.env.DEBUG === 1) return NextResponse.json({ message: 'Debug mode is enabled' }, { status: 200 });
 
     await connectToDatabase();
 
-    const sampleData = generateRandomData();
+    let buoy = await Buoy.findOne({ uid: BUOY_UID });
+    if (!buoy) {
+        // If not, create a new Buoy entry
+        buoy = new Buoy({
+            uid: BUOY_UID,
+            name: NAME,
+            location: {
+                type: 'Point',
+                coordinates: COORDS, // Example coordinates; adjust as needed
+            },
+            // Add other buoy details here
+        });
+        await buoy.save();
+    }
+
+    const sampleData = generateRandomData(buoy._id);
 
     try {
         const newEntry = new BuoyData(sampleData);

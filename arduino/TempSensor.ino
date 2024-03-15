ad775a333bb8ccef690469e4124632c6c517f963
j7zd <j7zd@github.com>
2024-03-15 02:57:38 +0200

    fix: temp

--- arduino/TempSensor.ino
+++ arduino/TempSensor.ino
@@ -1,4 +1,4 @@
-float readTemperature(sensorPin) {
+float readTemperature(int sensorPin) {
   float vout = analogRead(sensorPin) * 5.0 / 1024;
   return (vout - 0.5) * 100; // Convert to temperature
 }

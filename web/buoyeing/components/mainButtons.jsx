deeeb7a882810fd4cfaca3d979d9a96b233beaa7
Filostratos <ftitopoulos@gmail.com>
2024-03-15 06:06:38 +0200

    feature:main buttons redirect to location

--- web/buoyeing/components/mainButtons.jsx
+++ web/buoyeing/components/mainButtons.jsx
@@ -13,11 +13,12 @@ import {
 export default function MainButtons() {
     return (
         <div className="flex space-x-4">
-            {/* <Button
-            // variant="outline"
-            // className="px-6 py-3 border-gray-600 text-white hover:bg-gray-700 hover:border-gray-500"
-            > */}
-            <Card className="bg-gray-800 border-gray-700 shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 cursor-pointer w-auto">
+            <Card
+                className="bg-gray-800 border-gray-700 shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 cursor-pointer w-auto"
+                onClick={() => {
+                    window.location.href = "/docs"
+                }}
+            >
                 <CardHeader>
                     <CardTitle>Docs</CardTitle>
                 </CardHeader>
@@ -26,18 +27,14 @@ export default function MainButtons() {
                         Документацията на публичния API
                     </CardDescription>
                 </CardContent>
-                {/* <CardFooter>
-                    <Button
-                    // variant="outline"
-                    // className="px-6 py-3 border-gray-600 text-white hover:bg-gray-700 hover:border-gray-500"
-                    >
-                        View Docs
-                    </Button>
-                </CardFooter> */}
             </Card>
-            {/* </Button> */}
 
-            <Card className="bg-gray-800 border-gray-700 shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 cursor-pointer w-auto">
+            <Card
+                className="bg-gray-800 border-gray-700 shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 cursor-pointer w-auto"
+                onClick={() => {
+                    window.location.href = "/buoys"
+                }}
+            >
                 <CardHeader>
                     <CardTitle>All Buoys</CardTitle>
                 </CardHeader>
@@ -46,14 +43,6 @@ export default function MainButtons() {
                         Вижте всички свързани и активни шамандури към мрежата
                     </CardDescription>
                 </CardContent>
-                {/* <CardFooter>
-                    <Button
-                    // variant="outline"
-                    // className="px-6 py-3 border-gray-600 text-white hover:bg-gray-700 hover:border-gray-500"
-                    >
-                        Виж всички шамандури
-                    </Button>
-                </CardFooter> */}
             </Card>
 
         </div>

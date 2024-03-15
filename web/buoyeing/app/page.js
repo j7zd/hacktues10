09c816e70660d01c07cb42bddf7f71ba14a81204
Filostratos <ftitopoulos@gmail.com>
2024-03-15 16:31:05 +0200

    styling

--- web/buoyeing/app/page.js
+++ web/buoyeing/app/page.js
@@ -49,14 +49,14 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-between p-4 bg-gray-900 text-white">
-      <div className="flex flex-col md:flex-row w-full items-center md:justify-center space-y-4 md:space-y-0 md:space-x-10 pt-10">
-        <div className="w-full md:w-auto flex flex-col items-center text-center md:max-w-[50%]">
+      <div className="flex flex-col md:flex-row w-full items-center md:justify-center space-y-4 md:space-y-0 md:space-x-10 ">
+        <div className="w-full md:w-auto flex flex-col items-start text-center md:max-w-[50%]">
           {/* <h1 className="text-5xl font-bold text-indigo-400">Шамандурите На Бъдещето</h1> */}
           <div className={bebas.className}>
             {/* <h1 className="text-5xl font-bold text-indigo-400">Шамандурите На Бъдещето</h1> */}
             <h1 className="text-9xl font-bold text-indigo-400">Smart Buoy</h1>
           </div>
-          <p className="text-lg text-gray-300 mt-2">От нестойностен отпадък, в стойностна шамандура. <br />Разгледайте как нашите устройства правят <span className='font-bold text-indigo-400'>разликата</span>!</p>
+          <p className="text-lg text-gray-300 mt-2 italic text-gray-400/85">От нестойностен отпадък, в стойностна шамандура. <br />Разгледайте как нашите устройства правят <span className='font-bold text-indigo-400'>разликата</span>!</p>
         </div>
         <div className="w-full md:w-auto h-96 aspect-square flex items-center justify-center">
           {/* <World key={locations.length} globeConfig={globeConfig} data={locations} /> */}
@@ -64,7 +64,7 @@ export default function Home() {
         </div>
       </div>
 
-      <h2 className="my-4 text-lg text-gray-300 text-center">Разгледайте различните изследователски шамандури около Бургас!</h2>
+      <h2 className="my-4 text-xl text-gray-300 text-center ">Разгледайте различните изследователски шамандури около Бургас!</h2>
 
       <MainMap locations={locations} />
       {/* <HeatmapComponent data={heatmapLocations} /> */}

e350e2aa8e37ce56d0349413388f448d3ae452e8
j7zd <j7zd@github.com>
2024-03-15 04:10:58 +0200

    testing json

--- beach/flask/app.py
+++ beach/flask/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def receive_data():
-    data = request.get_data()
+    data = request.json()
     # if request.headers.get("api_key") != API_KEY:
     #     return "Unauthorized", 401
     print(data, flush=True)
